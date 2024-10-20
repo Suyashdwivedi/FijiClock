810c8334dc9db2b26c90800d3877be62ed26497e
Suyash Dwivedi <35916420+Suyashdwivedi@users.noreply.github.com>
2024-10-20 16:23:27 +0530

    Update FijiClock.js

--- FijiClock.js
+++ FijiClock.js
@@ -1,63 +1,64 @@
-//Originally from https://www.mediawiki.org/w/index.php?title=MediaWiki:Gadget-LocalLiveClock.js&action=raw&ctype=text/javascript
-// https://hif.wikipedia.org/wiki/sadasya:Suyash.dwivedi/FijiClock.js
-mw.loader.using(['mediawiki.util', 'mediawiki.api']).then( function () {
-var $target;
+// Originally from https://www.mediawiki.org/w/index.php?title=MediaWiki:Gadget-LocalLiveClock.js&action=raw&ctype=text/javascript
+// Updated and customized by Suyash Dwivedi (https://meta.wikimedia.org/wiki/User:Suyash.dwivedi)
 
-function showTime( $target ) {
-////////////////
-   var fijiOffset = 6.50; // Fiji is UTC+12 or +6 hrs 30 min (6.50 hrs) form India
-   var localDate = new Date();
-   var fijiDate = new Date(localDate.getTime() + (fijiOffset * 60 * 60 * 1000));
+mw.loader.using(['mediawiki.util', 'mediawiki.api']).then(function () {
+    var $target;
 
-   var fijiHours = fijiDate.getHours() % 12 || 12; // Display 12-hour format
-   var fijiMinutes = fijiDate.getMinutes().toString().padStart(2, "0");
-   var fijiSeconds = fijiDate.getSeconds().toString().padStart(2, "0");
+    function showTime($target) {
+        var localDate = new Date();
 
-   var fijiAmPm = fijiDate.getHours() < 12 ? "AM" : "PM";
+        // Fetch Fiji timezone (Pacific/Fiji) and adjust for DST automatically
+        var fijiTimezone = 'Pacific/Fiji';
+        var fijiDate = new Date(localDate.toLocaleString("en-US", { timeZone: fijiTimezone }));
 
-  var fijiTimeString =  "Fiji Time - " + fijiHours + ":" + fijiMinutes + ":" + fijiSeconds + " " + fijiAmPm;
-///////////////
-	$target.text( fijiTimeString );
+        var fijiHours = fijiDate.getHours() % 12 || 12; // 12-hour format
+        var fijiMinutes = fijiDate.getMinutes().toString().padStart(2, "0");
+        var fijiSeconds = fijiDate.getSeconds().toString().padStart(2, "0");
+        var fijiAmPm = fijiDate.getHours() < 12 ? "AM" : "PM";
 
+        var fijiTimeString = "Fiji Time - " + fijiHours + ":" + fijiMinutes + ":" + fijiSeconds + " " + fijiAmPm;
 
-	var ms = localDate.getUTCMilliseconds();
+        $target.text(fijiTimeString);
 
-	setTimeout( function () {
-		showTime( $target );
-	}, 1100 - ms );
-}
+        // Calculate the time remaining for the next second to keep the clock precise
+        var ms = 1000 - fijiDate.getMilliseconds();
 
-function liveClock() {
-	mw.util.addCSS( '#utcdate a { font-weight:bolder; font-size:120%; }' );
+        setTimeout(function () {
+            showTime($target);
+        }, ms);
+    }
 
-	if ( !window.UTCLiveClockConfig ) {
-		UTCLiveClockConfig = {};
-	}
-	var portletId = UTCLiveClockConfig.portletId || 'p-personal';
-	var nextNode = UTCLiveClockConfig.nextNodeId ? document.getElementById( UTCLiveClockConfig.nextNodeId ) : undefined;
-	var node = mw.util.addPortletLink(
-		portletId,
-		mw.util.getUrl( null, { action: 'purge' } ),
-		'',
-		'utcdate',
-		null,
-		null,
-		nextNode
-	);
-	if ( !node ) {
-		return;
-	}
-	$( node ).on( 'click', function ( e ) {
-		new mw.Api().post( { action: 'purge', titles: mw.config.get( 'wgPageName' ) } ).then( function () {
-			location.reload();
-		}, function () {
-			mw.notify( 'Purge failed', { type: 'error' } );
-		} );
-		e.preventDefault();
-	} );
+    function liveClock() {
+        mw.util.addCSS('#utcdate a { font-weight: bolder; font-size: 120%; }');
 
-	showTime( $( node ).find( 'a:first' ) );
-}
+        if (!window.UTCLiveClockConfig) {
+            UTCLiveClockConfig = {};
+        }
+        var portletId = UTCLiveClockConfig.portletId || 'p-personal';
+        var nextNode = UTCLiveClockConfig.nextNodeId ? document.getElementById(UTCLiveClockConfig.nextNodeId) : undefined;
+        var node = mw.util.addPortletLink(
+            portletId,
+            mw.util.getUrl(null, { action: 'purge' }),
+            '',
+            'utcdate',
+            null,
+            null,
+            nextNode
+        );
+        if (!node) {
+            return;
+        }
+        $(node).on('click', function (e) {
+            new mw.Api().post({ action: 'purge', titles: mw.config.get('wgPageName') }).then(function () {
+                location.reload();
+            }, function () {
+                mw.notify('Purge failed', { type: 'error' });
+            });
+            e.preventDefault();
+        });
 
-$( liveClock );
-} );
+        showTime($(node).find('a:first'));
+    }
+
+    $(liveClock);
+});
